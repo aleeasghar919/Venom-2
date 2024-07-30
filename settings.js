402a9b0aab8a2b1064775ca90cacb6ae5b24ead1
aleeasghar919 <138607010+aleeasghar919@users.noreply.github.com>
2024-07-30 09:21:49 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,7 +31,7 @@ global.prefa = ['','!','.','#','&']
 //false=disable and true=enable
 global.autoRecording = false //auto recording
 global.autoTyping = false //auto typing
-global.autorecordtype = true //auto typing + recording
+global.autorecordtype = false //auto typing + recording
 global.autoread = false //auto read messages
 global.autobio = false //auto update bio
 global.anti92 = false //auto block +92 
